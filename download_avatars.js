aa1d26d9d79950c4879b613f7982ee559478720c
Avleen Dhaliwal <avleen3@gmail.com>
2017-08-01 12:58:45 -0400

    Print each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -5,14 +5,28 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 var GITHUB_USER = "Avleen30";
 var GITHUB_TOKEN = "5c8c07a0740e3ff76a56cca63e4daa6d3b853ab7";
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, cb){
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
+
+  var options = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+  request(options, function(err, response, body) {
+    var parse= JSON.parse(body);
+    console.log()
+    cb(err, parse);
+  });
 
 }
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  result.forEach(function(user){
+    console.log(user.avatar_url);
+  })
 });
