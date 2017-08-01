3619d75ee74ccc9712e19eccb05d768dacb08c5a
Avleen Dhaliwal <avleen3@gmail.com>
2017-08-01 11:17:26 -0400

    adding calling (invoking) the getRepoContributors function using hard-coded values

--- download_avatars.js
+++ download_avatars.js
@@ -2,9 +2,15 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "Avleen30";
+var GITHUB_TOKEN = "5c8c07a0740e3ff76a56cca63e4daa6d3b853ab7";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+
 }
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
