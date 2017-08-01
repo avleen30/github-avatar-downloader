082ed36b089ac4bc985a1e1ceafe72e104f7faee
Avleen <avleen3@gmail.com>
2017-08-01 18:57:10 +0000

    cleaning my code

--- download_avatars.js
+++ download_avatars.js
@@ -5,45 +5,44 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 var GITHUB_USER = "Avleen30";
 var GITHUB_TOKEN = "5c8c07a0740e3ff76a56cca63e4daa6d3b853ab7";
 
-function getRepoContributors(repoOwner, repoName, cb){
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+function getRepoContributors(repoOwner, repoName, cb) {
+    var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+    console.log(requestURL);
 
-  var options = {
-  url: requestURL,
-  headers: {
-    'User-Agent': 'request'
-  }
-};
+    var options = {
+        url: requestURL,
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
 
-  request(options, function(err, response, body) {
-    var parse= JSON.parse(body);
-    console.log()
-    cb(err, parse);
-  });
+    request(options, function(err, response, body) {
+        var parse = JSON.parse(body);
+        console.log()
+        cb(err, parse);
+    });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  result.forEach(function(user){
-    user.avatar_url;
-    downloadImageByURL(user.avatar_url, 'avatars/' + user.login + ".jpg")
+function callback(err, result) {
+    result.forEach(function(user) {
+        user.avatar_url;
+        downloadImageByURL(user.avatar_url, 'avatars/' + user.login + ".jpg")
     })
-});
-
+}
 
 
 function downloadImageByURL(url, filePath) {
-  var request = require('request');
-  var fs = require('fs');
+    var request = require('request');
+    var fs = require('fs');
 
-  request.get(url)
-         .on('error', function (err){
-          console.log(err);
-          })
-         .on('response', function (response) {
-          console.log('Response Status Code: ', response.statusCode);
-          })
-         .pipe(fs.createWriteStream(filePath));
-    }
+    request.get(url)
+        .on('error', function(err) {
+            console.log(err);
+        })
+        .on('response', function(response) {
+            console.log('Response Status Code: ', response.statusCode);
+        })
+        .pipe(fs.createWriteStream(filePath));
+}
 
-
+getRepoContributors(process.argv[2], process.argv[3], callback);
